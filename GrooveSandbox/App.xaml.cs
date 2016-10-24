867c9074d6dd15be97105945ad2390a847a4247b
Johan Lindfors <johan.lindfors@coderox.se>
2016-10-24 22:01:05 +0200

    Initial commit

---
+++ GrooveSandbox/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Template10.Common;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace GrooveSandbox
+{
+    sealed partial class App
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        public override Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            var nav = NavigationServiceFactory(BackButton.Attach, ExistingContent.Include);
+            Window.Current.Content = new Shell(nav);
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task OnStartAsync(BootStrapper.StartKind startKind, IActivatedEventArgs args)
+        {
+            NavigationService.Navigate(typeof(Views.MainPage));
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ GrooveSandbox/Shell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Template10.Services.NavigationService;
+
+namespace GrooveSandbox
+{
+    public sealed partial class Shell
+    {
+        public Shell(INavigationService navigationService)
+        {
+            this.InitializeComponent();
+            Menu.NavigationService = navigationService;
+        }
+    }
+}
